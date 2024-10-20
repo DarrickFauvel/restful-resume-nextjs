066d9e4ed791bd76ec98d25be9e6d5debab00291
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-10-20 17:30:54 -0400

    Create header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,30 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Darrick Fauvel</h1>
+        <span>Software Developer</span>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#Experience">Experience</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Header
