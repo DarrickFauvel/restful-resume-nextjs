9c8a6859a898a006dd2becd0060c2f2269d4e17f
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-11-07 04:02:06 -0500

    Create app config file

---
+++ app/config.ts
@@ -0,0 +1,34 @@
+export const contactInfo = {
+  name: "Darrick Fauvel",
+  title: "Software Developer",
+  email: "darrick@darrickdevelops.com",
+  city: "Lowell",
+  state: {
+    long: "Massachusetts",
+    short: "MA",
+  },
+  summary: "Experienced developer seeking a challenging role.",
+}
+
+export const sections = [
+  {
+    title: "About",
+    id: "about",
+  },
+  {
+    title: "Projects",
+    id: "projects",
+  },
+  {
+    title: "Experience",
+    id: "experience",
+  },
+  {
+    title: "Education",
+    id: "education",
+  },
+  {
+    title: "Contact",
+    id: "contact",
+  },
+]
