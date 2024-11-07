bee194bf55ffc5ff7fd130f34ec3df63612c54f1
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-11-07 03:31:49 -0500

    Apply components

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
