a04df8e828b56f2c9cc80583902b1756289db5cb
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-11-10 13:32:50 -0500

    Add flex styling

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,13 +3,13 @@ import { sections } from "../config"
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className="flex">
+      <div className="flex-grow">
         <h1>{contactInfo.name}</h1>
         <span>{contactInfo.title}</span>
       </div>
       <nav>
-        <ul>
+        <ul className="flex flex-col">
           {sections.map(({ title, id }) => (
             <li key={id}>
               <a href={`#${id}`}>{title}</a>
